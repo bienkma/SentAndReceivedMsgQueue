951c99097ab2041a653b80f089e97b0c0d9195d4
bienkma <bientd88@gmail.com>
2018-04-15 13:54:06 +0700

    use epool for kafka consummer

--- app/handler/Receiver.go
+++ app/handler/Receiver.go
@@ -8,14 +8,17 @@ import (
 func (kk *NewKafka) Reciver(ctx BaseHandler) view.ApiResponse {
 	ctx.request.ParseForm()
 	topic, _ := ctx.request.URL.Query()["topic"]
+
 	if len(topic) != 0 {
-		if err := kk.NewConsumer.SubscribeTopics([]string{topic[0], "^aRegex.*[Tt]opic"}, nil); err != nil {
+		err := kk.NewConsumer.SubscribeTopics(topic, nil)
+		defer kk.NewConsumer.Close()
+		if err != nil {
 			return view.ApiResponse{Status: http.StatusBadRequest, Msg: "Not msg"}
 		}
 		defer kk.NewConsumer.Close()
-		msg, err := kk.NewConsumer.ReadMessage(1)
+		msg := kk.NewConsumer.Poll(100)
 		if err == nil {
-			return view.ApiResponse{Status: http.StatusOK, Msg: string(msg.Value)}
+			return view.ApiResponse{Status: http.StatusOK, Msg: string(msg.String())}
 		}
 	}
 	return view.ApiResponse{Status: http.StatusBadRequest, Msg: "Not sent"}
